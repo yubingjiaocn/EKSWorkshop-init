dc9669be749c15b2e3ab7cb9ad45f2cca1136edf
Yu Bingjiao <yubingjiaocn@users.noreply.github.com>
2022-10-12 15:46:02 +0800

    Add CLUSTER_ENDPOINT to ~/.bash_profile

--- install_deps.sh
+++ install_deps.sh
@@ -24,10 +24,12 @@ export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
+export CLUSTER_ENDPOINT="$(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output text)"
 
 echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
 echo "export AWS_ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
 echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+echo "export CLUSTER_ENDPOINT=${CLUSTER_ENDPOINT}" | tee -a ~/.bash_profile
 
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
