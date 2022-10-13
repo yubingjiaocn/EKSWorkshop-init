4b30e7fd8c389aad3c8305e97f5e94b9f22ef1a1
Yu Bingjiao <yubingjiaocn@users.noreply.github.com>
2022-10-13 13:39:14 +0800

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 echo "Upgrading AWS CLI"
-sudo yum remove aws-cli
+sudo yum remove aws-cli -y
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
